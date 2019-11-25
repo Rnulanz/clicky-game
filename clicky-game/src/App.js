df66c21c6f058891ac5eba5ec9433ad7d588b8bf
Dick James <rnulanz@gmail.com>
2019-11-24 21:42:09 -0600

    added functionality to the game and did README

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Nav from "./components/Nav";
+import Wrapper from "./components/Wrapper";
+import GameInstructions from "./components/GameInstructions";
+import friends from "./friends.json";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    score: 0,
+    highScore: 0,
+    friends: friends
+  };
+
+  randomRender = id => {
+    this.state.friends.map((image) => {
+      if (image.id === id) {
+        if (image.cliked) {
+          alert('YOU LOST!! This card was previously selected.');
+          this.setState({})
+          this.resetGame();
+          return false;
+        }
+        else {
+          this.updateScore();
+          image.cliked = true;
+        }
+        if (this.state.score >= this.state.highScore) {
+          this.newHighScore();
+        }
+      }
+    });
+  }
+
+  randomOrganize = (array) => {
+    let copy = [], n = array.length, i;
+    while (n) {
+      i = Math.floor(Math.random() * array.length);
+      if (i in array) {
+        copy.push(array[i]);
+        delete array[i];
+        n--;
+      }
+    }
+    this.setState({ friends: copy });
+  }
+
+  updateScore = () => {
+    this.setState((newState) => ({
+      score: newState.score + 1
+    }), () => this.winning())
+  }
+
+  newHighScore = () => {
+    this.setState((newState) => ({
+      highScore: newState.score
+    }))
+  }
+
+  winning = () => {
+    if (this.state.score === this.state.friends.length) {
+      alert('YOU WIN!')
+      this.setState({});
+      this.resetGame();
+    }
+    else {
+      setTimeout(() => {
+        this.randomOrganize(this.state.friends)
+      }, 500);
+    }
+  }
+
+  resetGame = () => {
+    this.state.friends.forEach((image) => {
+      image.cliked = false;
+    })
+    this.setState({ score: 0 })
+  }
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+          <Nav score={this.state.score} highScore={this.state.highScore} />
+          <GameInstructions />
+        {this.state.friends.map(friend => {
+          return <FriendCard
+            {...friend}
+            key={friend.id}
+            randomRender={this.randomRender}
+            randomOrganize={() => this.randomOrganize(this.state.friends)}
+          />;
+        })}
+      </Wrapper>
+  )};
 }
 
 export default App;
